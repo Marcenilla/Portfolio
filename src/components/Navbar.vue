27953ebe1b54cf745587b6ed456ac142c5bf8a45
Mitchap <mitchbarcenilla4@gmail.com>
2025-03-10 18:20:42 +0800

    updated portfolio

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,24 @@
 <template>
 
 <!-- big screen navbar -->
-<div class="hidden md:block">
-    <nav class="bg-neutral-50 flex flex-row justify-between px-10 py-7 fixed top-o w-screen">
+<div>
+    <nav class="bg-neutral-50 flex flex-row justify-between px-10 py-7 fixed top-o w-screen z-99 ">
         <a href="#"><img src="/images/Mitch-Logo.jpg" alt="Mitch Barcenilla Logo" class="w-52"></a>
         <ul class="flex flex-row gap-10 font-archivo font-medium">
-            <li><a href="" class="hover-slider">Home</a></li>
-            <li><a href="" class="hover-slider">About Me</a></li>
-            <li><a href="" class="hover-slider">Experience</a></li>
-            <li><a href="" class="hover-slider">Contact</a></li>
-            <li><a href="" class="hover-slider">Resume</a></li>
+            <li ><a href="#" class="hover-slider">Home</a></li>
+            <li ><a href="#about" class="hover-slider">About Me</a></li>
+            <li ><a href="#" class="hover-slider">Experience</a></li>
+            <li ><a href="#" class="hover-slider">Contact</a></li>
+            <li ><a href="#" class="hover-slider">Resume  <i class="fa-solid fa-arrow-up-right-from-square text-sm ms-1"></i></a></li>
         </ul>
     </nav>
 </div>
 <!-- small screen sidebar -->
 
 </template>
+
+
+<script>
+
+    
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,53 +1,66 @@
 <template >
 <!-- hero section  -->
 
-    <section class=" mx-auto px-10 md:px-56 flex flex-col justify-center ">
-        <div class="flex flex-col offset-2">
-            <p class="font-medium font-archivo text-sm md:text-2xl md:ms-20">HEY! I'm <span class="text-neutral-100 bg-[#C50000] px-1 mx-2"> MITCH.</span></p>
-        </div>
+    <section class=" mx-auto px-10 md:px-56">
+
         <div class="text-center flex flex-col justify-center items-center font-playfair-display font-bold uppercase">
+            <div class="me-40 md:me-[50vw]">
+            <p class="font-medium font-archivo text-sm md:text-2xl">HEY! I'm <span class="text-neutral-100 bg-[#C50000] px-1 ms-1">MITCH.</span></p>
+            </div>
+
             <h1 class="text-[2rem] md:text-[6rem] text-[#1b1b1b] ">Full Stack</h1>
-            <div class="w-56 md:w-[50vw] h-0.5 bg-neutral-900 mx-auto"></div>
-            <h1 class="text-[2rem] md:text-[6rem]  text-[#ED1B24] ">Web Developer</h1>
+            <div class="w-56 md:w-[55rem] h-0.5 bg-neutral-900 mx-auto"></div>
+            <h1 class="text-[2rem] md:text-[6rem] text-[#ED1B24] ">Web Developer</h1>
         </div>
         <div class="text-center">
         <h2 class="text-md md:text-2xl text-neutral-500 font-karla">From Bacolod City, Philippines</h2>
             <div class="flex flex-col md:flex-row justify-center items-center gap-5 mt-10">
-                    <p class="font-archivo md:font-bold uppercase text-[12px] md:text-lg">Learn more about me</p>
-                    <a href="#" type="button" class="uppercase bg-neutral-900 hover:bg-red-600 transition duration-300 ease-in-out px-5 py-1 md:px-10 md:py-2 rounded-full font-medium  text-neutral-100 text-[12px] md:text-lg" >Let's go!</a>
+                    <p class="font-archivo md:font-bold uppercase text-[12px] md:text-lg ">Learn more about me</p>
+                    <a href="#about" type="button" class=" uppercase border-0 bg-neutral-900 hover:bg-red-600 transition duration-300 ease-in-out px-5 py-1 md:px-10 md:py-2 rounded-full font-medium  text-neutral-100 text-[12px] md:text-lg" >Let's go!</a>
             </div>
     </div>
     </section>
 
 <!-- about me -->
     <div>
-       <div class="flex flex-col justify-center text-center mt-40">
+       <div class="flex flex-col justify-center text-center mt-20 md:mt-40">
         <i class="fa-solid fa-xmark text-[#ED1B24]"></i>
-        <div class="h-20 w-0.5 bg-neutral-900 mx-auto mt-4 rounded"></div>
+        <div class="h-10 md:h-20 w-0.5 bg-neutral-900 mx-auto mt-4 rounded"></div>
        </div>
     </div>
 
 
-    <section>
-        <div class="flex flex-col mx-56">
-            <div class="flex flex-col font-bold font-archivo text-[2rem] ">
-            <h1 class="text-neutral-800">ABOUT ME:</h1>
-            <h1 class="text-[#ED1B24]">MITCH BARCENILLA.</h1>
-        </div>
-        </div>
+    <section id="about" class="flex justify-center pt-20 md:pt-40">
+        
+        <div class="flex flex-col md:flex-row ">
 
+            <div class="flex flex-col font-archivo  z-12 md:me-[-150px]">
+                <h1 class="text-2xl md:text-[4rem] text-neutral-800 font-medium">ABOUT ME:</h1>
+                <h1 class="text-2xl md:text-[4rem] text-[#ED1B24] font-bold">MITCH BARCENILLA.</h1>
+                <div class="h-2 w-20 md:h-4 md:w-52 bg-neutral-800"></div>
 
-        <div class="relative w-94 h-94 bg-red-600 rounded-full mx-auto right-[-5%]">
-
-        </div>
-
-        <div class="absolute">
-                <img src="/images/Mitch-B&W.jpg" alt="">
+            <div class="mt-10 font-archivo text-lg w-56 md:w-96 leading-10">
+                <p class=""> I enjoy creating visually appealing web designs and developing user-friendy software solutions. 
+                    <br><br>
+                   Outside of work, I read novels, exercise, brainstorm new ideas for my start up brand, and spend time with my family. </p>
             </div>
+            </div>
+            
+            <div class="flex flex-row items-center mt-10 md:mt-auto">
+                <div class="h-40 w-20 md:h-72 md:w-36 bg-red-600 rounded-tl-full rounded-bl-full"></div>
+                <img src="/images/Mitch-B&W.jpg" class="w-40 md:w-92 ms-[-40px] z-10" alt="Mitch dressed in black in black and white filter">
+            
+            </div>
+            
+        </div>
+
     </section>
 </template>
 
-<script></script>
+<script>
+
+
+</script>
 
 <style>
 </style>
